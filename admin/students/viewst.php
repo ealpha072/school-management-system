37101f500a525c101b2fb27c9ddeeab88f8c0f3f
ealpha072 <ealpha072@gmail.com>
2021-09-29 12:13:33 +0300

    Created new files for this folder

---
+++ admin/students/viewst.php
@@ -0,0 +1,12 @@
+<?php require "../shared/home.php"; ?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h4>View Students</h4>
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+</div>
