cc8b202113c3a08e1a351f87e25b0102dcf79caf
ealpha072 <ealpha072@gmail.com>
2021-09-30 11:47:18 +0300

    Complete page for managing role

--- admin/roles/managerole.php
+++ admin/roles/managerole.php
@@ -2,5 +2,39 @@
 
 <div class="container-fluid">
     <div class="card">
+        <div class="card-header">
+            <h5>Manage Roles</h5>
+        </div>
+        <div class="card-body">
+            <div class="form-row">
+                <div class="col">
+                    <select name="" id="" class="form-control">
+                        <!--consider populationg with php-->
+                        <option value="" selected disabled>Role to manage</option>
+                        <option value="">Accountant</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <form action="" method="post">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Search Role" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="basic-addon2"><button class="btn btn-primary">Search</button></span><!--FIX THIS ISSUE-->
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr>
+            <div class="card results-holder">
+                <div class="card-header">
+                    <h5>Results</h5>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
