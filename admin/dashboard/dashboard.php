5474edf42a0101949030429a5d8f35150d57bf22
ealpha072 <ealpha072@gmail.com>
2021-09-30 20:51:04 +0300

    Dashboard page

---
+++ admin/dashboard/dashboard.php
@@ -0,0 +1,77 @@
+<?php require "../shared/home.php"?>
+
+<div class="container-fluid">
+    <div class="card">
+        <div class="card-header">
+            <h6>School Infomation</h6>
+        </div>
+        <div class="card-body">
+            <div class="form-row">
+                <div class="col">
+                    <div class="card text-white bg-primary">
+                        <div class="card-body">
+                            <h1>10</h1>
+                            <h4>Students</h5>
+                            <h6>Total Students</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-secondary">
+                        <div class="card-body">
+                            <p>Some text</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-success">
+                        <div class="card-body">
+                            <p>Some text</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-dark">
+                        <div class="card-body">
+                            <p>Some text</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="form-row">
+                <div class="col">
+                    <div class="card text-white bg-warning">
+                        <div class="card-body">
+                            <p>Some text</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-danger">
+                        <div class="card-body">
+                            <p>Some text</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-info">
+                        <div class="card-body">
+                            <p>Some text</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-black bg-light">
+                        <div class="card-body">
+                            <p>Some text</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+        </div>
+    </div>
+</div>
