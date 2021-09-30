6346c6788ea11e30b4f60c801fdce4de6168cc10
ealpha072 <ealpha072@gmail.com>
2021-09-30 09:10:36 +0300

    Form for managing students

--- admin/students/managest.php
+++ admin/students/managest.php
@@ -1,12 +1,42 @@
 <?php require "../shared/home.php"; ?>
 
-<div class="container">
+<div class="container-fluid">
     <div class="card">
         <div class="card-header">
-            <h4>View Students</h4>
+            <h4>Manage Students</h4>
         </div>
         <div class="card-body">
-            
+            <form action="" method="post">
+                <div class="form-row">
+                    <div class="col">
+                        <select name="" id="" class="form-control">
+                            <option value="" selected disabled>Choose a class</option>
+                            <option value="">Form 1</option>
+                            <option value="">Form 2</option>
+                        </select>
+                    </div>
+                    <div class="col">
+                        <select name="" id="" class="form-control">
+                            <option value="" selected disabled>Choose a stream</option>
+                            <option value="">Form 1</option>
+                            <option value="">Form 2</option>
+                        </select>
+                    </div>
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary" onclick="">Manage</button>
+                    </div>
+                </div>
+            </form>
+            <div class="card result-holder">
+                <div class="card-header">
+                    <h5>Results</h5>
+                </div>
+                <div class="card-body">
+                    <!--THIS SECTION HOLDS THE RESULTS OF THE SEARCHED ITEM--
+                    ---------------------------------------------------------
+                    ---------------------------------------------------->
+                </div>
+            </div>
         </div>
     </div>
 </div>
