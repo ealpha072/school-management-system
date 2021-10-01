7efb08661efd7b2d9d98e3d983e7850e8bc76e97
ealpha072 <ealpha072@gmail.com>
2021-10-01 21:38:05 +0300

    Refactored the login function

--- admin/shared/config.php
+++ admin/shared/config.php
@@ -22,6 +22,7 @@
     //SQL statements
     $sql1 = $conn->prepare("SELECT * FROM `admin` WHERE username=? AND password =? ");
     
+    
 
 
     if(isset($_POST['login']) && $_SERVER["REQUEST_METHOD"]=="POST"){ 
@@ -31,45 +32,58 @@
 
 
     function login() {
-        global $sql1;
+        global $sql1, $loginerror;
         $username = $_POST["username"];
         $password = $_POST["password"];
-        $sql1->execute(array($username, $password));
-        $results = $sql1->fetchAll(PDO::FETCH_ASSOC);
 
-        if($username ==='' || $password===''){
-            array_push($loginerror, 'Fields cannot be empty');
-            exit("Error logging you in, please check your credentials and try again");
-        }
+        if(count($loginerror)==0){
+            $sql1->execute(array($username, $password));
+            $results = $sql1->fetchAll(PDO::FETCH_ASSOC);
 
-        if($username===$results[0]['username'] && $password===$results[0]['password']){
-            $_SESSION['loggedin'] = True;
-            $_SESSION['msg'] = 'Successful login';
-            require "../shared/header.php";
-            require "../shared/footer.php";
-            echo
-            '<div class="container h-100">
-                <div class="row h-100 justify-content-center align-items-center">
-                    <div class="spinner-border" role="status" style="width: 3rem; height: 3rem;">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                    <div>
-                        <h1>Loading, please wait...</h1>
-                    </div>  
-                </div>
-            </div>
-            ';
-            header("refresh:3;url='../dashboard/dashboard.php'");
-            exit();
-        }else{
-            array_push($loginerror, "Incorrect password or username, try again");
+            if(count($results)==1){
+                if($username===$results[0]['username'] && $password===$results[0]['password']){
+                    $_SESSION['loggedin'] = True;
+                    $_SESSION['msg'] = 'Successful login';
+                    require "../shared/header.php";
+                    require "../shared/footer.php";
+                   echo
+                    '<div class="container h-100">
+                        <div class="row h-100 justify-content-center align-items-center">
+                            <div class="spinner-border" role="status" style="width: 3rem; height: 3rem;">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                            <div>
+                                <h1>Loading, please wait...</h1>
+                            </div>  
+                        </div>
+                    </div>';
+                    echo count($results);
+                    header("refresh:3;url='../dashboard/dashboard.php'");
+                    exit();
+                }
+            }else{
+                ///when wrong username or password...
+                echo 'No record found';
+                array_push($loginerror, "Invalid username or password, Try again");
+            }
         }
         
     }
 
-    function addStudent() {
-
+    function displayLoginError() {
+        global $loginerror;
+        
+        if(count($loginerror) > 0){
+            echo '<div class="invalid-feedback">';
+                foreach($loginerror as $error){
+                    echo $error .'<br>';
+                }
+            echo'</div>';
+        }
     }
 
+    function newStudent() {
+
+    }
 
 ?>
