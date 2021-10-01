3d952c2feefc0da737129eda3b408dcdc11a1eb0
ealpha072 <ealpha072@gmail.com>
2021-10-01 21:48:30 +0300

    Name attribute to input tags

--- admin/students/addst.php
+++ admin/students/addst.php
@@ -17,7 +17,7 @@
                         <div class="form-row">
                             <div class="col">
                                 <label for="admission-number">Adm Number<sup>*</sup></label>
-                                <input type="text" placeholder="Adm Number" class="form-control">
+                                <input type="text" placeholder="Adm Number" class="form-control" name="adm_num">
                             </div>
                             <div class="col">
                                 <label for="form">Form<sup>*</sup></label>
@@ -63,21 +63,21 @@
                         <div class="form-row">
                             <div class="col">
                                 <label for="first-name">First Name<sup>*</sup></label>
-                                <input type="text" placeholder="First Name" class="form-control">
+                                <input type="text" placeholder="First Name" class="form-control" name="first-name">
                             </div>
                             <div class="col">
                                 <label for="middle-name">Middle Name<sup>*</sup></label>
-                                <input type="text" placeholder="Middle Name" class="form-control">
+                                <input type="text" placeholder="Middle Name" class="form-control" name="middle-name">
                             </div>
                             <div class="col">
                                 <label for="last-name">Last Name<sup>*</sup></label>
-                                <input type="text" placeholder="Last Name" class="form-control">
+                                <input type="text" placeholder="Last Name" class="form-control" name="last-name">
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="col">
                                 <label for="gender">Gender<sup>*</sup></label>
-                                <select name="" id="" class="form-control">
+                                <select name="gender" id="" class="form-control">
                                     <option value="" selected disabled>Choose Gender</option>
                                     <option value="">Male</option>
                                     <option value="">Female</option>
@@ -85,17 +85,17 @@
                             </div>
                             <div class="col">
                                 <label for="Nationality">Nationality<sup>*</sup></label>
-                                <input type="text" placeholder="Nationality" class="form-control">
+                                <input type="text" placeholder="Nationality" class="form-control" name="nationality">
                             </div>
                             <div class="col">
                                 <label for="county">County<sup>*</sup></label>
-                                <input type="text" placeholder="County" class="form-control">
+                                <input type="text" placeholder="County" class="form-control" name="county">
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="col">
                                 <label for="photo">Passport photo <sup>*</sup></label>
-                                <input type="file" placeholder= "Upload a passport file" class="form-control">
+                                <input type="file" placeholder= "Upload a passport file" class="form-control" name="student_photo">
                             </div>
                         </div>
                     </div>
@@ -111,25 +111,25 @@
                         <div class="form-row">
                             <div class="col">
                                 <label for="first-name">First Name<sup>*</sup></label>
-                                <input type="text" placeholder="First Name" class="form-control">
+                                <input type="text" placeholder="First Name" class="form-control" name="first-name">
                             </div>
                             <div class="col">
                                 <label for="mid-name">Mid Name<sup>*</sup></label>
-                                <input type="text" placeholder="Mid Name" class="form-control">
+                                <input type="text" placeholder="Mid Name" class="form-control" name="mid-name">
                             </div>
                             <div class="col">
                                 <label for="last-name">Last Name<sup>*</sup></label>
-                                <input type="text" placeholder="Last Name" class="form-control">
+                                <input type="text" placeholder="Last Name" class="form-control" name="last-name">
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="col">
                                 <label for="email">Email <sup>*</sup></label>
-                                <input type="email" placeholder="Email Address" class="form-control">
+                                <input type="email" placeholder="Email Address" class="form-control" name="email">
                             </div>
                             <div class="col">
                                 <label for="phone-number">Phone Number <sup>*</sup></label>
-                                <input type="text" placeholder="Phone Number" class="form-control">
+                                <input type="text" placeholder="Phone Number" class="form-control" name="phone-number">
                             </div>
                         </div>
                     </div>
