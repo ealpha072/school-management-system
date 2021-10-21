88ee885251878357793c25c1cdc521c5370cbfa9
ealpha072 <ealpha072@gmail.com>
2021-10-21 09:40:24 +0300

    Edit staff details

---
+++ admin/staff/updatestaff.php
@@ -0,0 +1,63 @@
+<?php
+    require '../shared/home.php';
+
+    if(isset($_GET['id']) && isset($_GET['action']) && $_GET['action']=='edit'){
+        $id = $_GET['id'];
+        $query = $db->prepare('select * from support_staff where id=:id');
+        $query->execute(array(':id'=>$id));
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $results = $results[0];
+    }
+?>
+
+<div class="container-fluid">
+    <di class="card">
+        <div class="card-header">
+            <h4>Update teacher Records: <?php echo $results['first_name'].' '.$results['mid_name'].' '.$results['last_name'];?></h4>
+        </div>
+        <div class="card-body">
+            <form action="" method="post">                
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label col-form-label-sm">Name</label>
+                    <div class="col-sm-4">
+                        <input 
+                            type="text" class="form-control form-control-sm" id="name" 
+                            value="<?php echo $results['first_name'].' '.$results['mid_name'].' '.$results['last_name'];?>" 
+                            readonly
+                        >
+                    </div>
+                    <label for="" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
+                    <div class="col-sm-4">
+                        <input 
+                            type="email" class="form-control form-control-sm" id="" 
+                            value="<?php echo $results['email']?>" 
+                            readonly
+                        >
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label col-form-label-sm">Phone Number</label>
+                    <div class="col-sm-4">
+                        <input 
+                            type="number" class="form-control form-control-sm" id="name" required
+                            value="<?php echo $results['phone_number']?>"
+                        >
+                    </div>
+                    <label for="name" class="col-sm-2 col-form-label col-form-label-sm">Role</label>
+                    <div class="col-sm-4">
+                        <select name="" id="" class="form-control form-control-sm" required>
+                            <option value="" selected disabled><?php echo $results['role']?></option>
+                            <?php 
+                                //roles population
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary" name="">Update</button>
+                </div>
+            </form>
+        </div>
+    </di>
+</div>
