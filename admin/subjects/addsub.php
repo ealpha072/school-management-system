979f393acbfeb34ce7eda3ae332febd9c62538e1
ealpha072 <ealpha072@gmail.com>
2021-10-22 10:18:53 +0300

    Use small classes on the add subject form

--- admin/subjects/addsub.php
+++ admin/subjects/addsub.php
@@ -12,7 +12,7 @@
         <div class="card-body">
             <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
                 <?php
-                    echo showSuccessMessage();
+                    showSuccessMessage();
 
                     if(isset($add_subject_error) && !empty($add_subject_error)){
                         displayErrors($add_subject_error);
@@ -23,14 +23,16 @@
                         <h5>Subject Info</h5>
                     </div>
                     <div class="card-body">
-                        <div class="form-row">
-                            <div class="col">
-                                <label for="name">Name</label>
-                                <input type="text" placeholder="Name of subject" class="form-control" name="subject-name" required="">
+                        <div class="form-group row">
+
+                            <label for="name" class="col-sm-2 col-form-label col-form-label-sm">Name</label>
+                            <div class="col-sm-4">
+                                <input type="text" placeholder="Name of subject" class="form-control form-control-sm" name="subject-name" required="">
                             </div>
-                            <div class="col">
-                                <label for="teacher">Head of Subject</label>
-                                <select name="hos" id="" class="form-control" required="">
+
+                            <label for="teacher" class="col-sm-2 col-form-label col-form-label-sm">Head of Subject</label>
+                            <div class="col-sm-4">
+                                <select name="hos" id="" class="form-control form-control-sm" required="">
                                     <option value="" disabled selected="">--Head of subject--</option>
                                     <?php
                                         $results = $select_teacher->fetchAll(PDO::FETCH_ASSOC);
@@ -43,18 +45,20 @@
                                 </select>
                             </div>
                         </div>
-                        <div class="form-row">
-                            <div class="col">
-                                <label for="subject-type">Subject type</label>
-                                <select name="subject-type" id="" class="form-control" required="">
+                        <div class="form-group row">
+
+                            <label for="subject-type" class="col-sm-2 col-form-label col-form-label-sm">Subject type</label>
+                            <div class="col-sm-4">
+                                <select name="subject-type" id="" class="form-control form-control-sm" required="">
                                     <option value="" selected disabled>Choose subject type</option>
                                     <option value="compulsory">Complusory</option>
                                     <option value="elective">Elective</option>
                                 </select>
                             </div>
-                            <div class="col">
-                                <label for="department">Department</label>
-                                <select name="department" id="" class="form-control" required="">
+                            
+                            <label for="department" class="col-sm-2 col-form-label col-form-label-sm">Department</label>
+                            <div class="col-sm-4">
+                                <select name="department" id="" class="form-control form-control-sm" required="">
                                     <option value="" selected disabled>Choose Department</option>
                                     <?php
                                         displayMenu($select_department, 'dpt_name');
@@ -64,7 +68,7 @@
                         </div>
                         <hr>
                         <div class="submit-button">
-                            <button class="btn btn-primary" name="add-subject" type="submit">Add subject</button>
+                            <button class="btn btn-primary btn-sm" name="add-subject" type="submit">+Add subject</button>
                         </div>
                     </div>
                 </div>
