4964975b55a9d7ed92683ad346d3b8be4acfffd9
ealpha072 <ealpha072@gmail.com>
2021-11-12 15:07:55 +0300

    New connection to database

--- admin/shared/oop/config.php
+++ admin/shared/oop/config.php
@@ -1,28 +1,39 @@
 <?php
 
-use function PHPSTORM_META\type;
+    session_start();
 
-session_start();
-
-    class Database{
+    class Database
+    {
         private $conn = null;
-        public $tablename = 'admin';
-
-        public function __construct()
+        public function getConn()
         {
             try {
                 $this->conn = new PDO('sqlite:../../mydatabase.db');
-                echo "Connection successfull";
+                echo "Good conn";
             } catch (Exception $e) {
                 //throw $th;
                 throw new Exception($e->getMessage());
             }
+            return $this->conn;
+        }
+    }
+
+    class Students
+    {   
+        private $conn;
+        public $tablename = 'students';
+        public $adm;
+        public $
+
+
+        public function __construct($db)
+        {
+            $this->conn = $db;
         }
 
-        
     }
 
     $database = new Database();
-
-
+    $database->getConn();
+    $newst = new Students($database);
 ?>
